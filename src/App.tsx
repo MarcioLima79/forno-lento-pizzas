d769e73d50203d4e7fcfafea3dc865950caeb50c
Marcio Lima <marciovl.2022@gmail.com>
2026-09-11 11:54:31 -0300

    feat: add responsive process section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import Header from "./sections/Header/Header"
-import Hero from "./sections/Hero/Hero"
+import Header from "./sections/Header/Header";
+import Hero from "./sections/Hero/Hero";
+import Process from "./sections/Process/Process";
 
 function App() {
   return (
     <main>
       <Header />
       <Hero />
+      <Process />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
